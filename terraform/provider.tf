e5104b3913481fc2cebe2d59384c127957149e34
Khingarthur <arthurfrederick03@gmail.com>
2023-10-19 14:14:38 +0000

    update 2

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,9 @@
 terraform {
-    backend "s3" {
-        bucket = "infra/tfstate"
-    }
+   backend "s3" {
+    bucket = "Practice"
+    key    = "infra/terraform.tfstate"
+    region = "us-east-1"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
